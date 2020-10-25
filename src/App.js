993b8a4102dc511265f2bf85cf9bdfc7ed70c24b
Sumant Katti <65009624+sumantk33@users.noreply.github.com>
2020-10-26 00:24:04 +0530

    Converted App into an arrow function

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from "react";
 import { Container } from "react-bootstrap";
 import TodoList from "./components/TodoList";
 
-function App() {
+const App = () => {
   return (
     <div className='py-5'>
       <Container>
