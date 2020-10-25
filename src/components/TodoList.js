ae1d4e6aadf122812b439a792ade475b47f7740e
sumantk33 <sumantk33@gmail.com>
2020-10-25 13:26:44 +0530

    File cleanup and Todolist setup

---
+++ src/components/TodoList.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const TodoList = () => {
+  return (
+    <div>
+      <h1 className='text-center' style={{ fontSize: "60px" }}>
+        Todo-List
+      </h1>
+    </div>
+  );
+};
+
+export default TodoList;
